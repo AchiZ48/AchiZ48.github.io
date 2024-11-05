3681ccf14b7d9d844eda4b1085befe60b5a9e60a
AchiZ48 <74602192+AchiZ48@users.noreply.github.com>
2024-11-05 13:14:40 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('I LOVE U').split('')
+    const titles = ('แฮปปี้เบิร์ดเดย์ค้าบหมูอ้วนสุดที่รัก').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
